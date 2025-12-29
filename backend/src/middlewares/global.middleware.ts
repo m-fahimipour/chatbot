b4f2f70c53d9b05648b0bb3ed17f0a4b8b380a13
Mostafa <mostafa.fahimipour@gmail.com>
2025-12-29 11:30:15 +0330

    fix: cors error was fixed

--- backend/src/middlewares/global.middleware.ts
+++ backend/src/middlewares/global.middleware.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import cors from "cors";
 
 import type { Express } from "express";
 
 export function globalMiddleware(app: Express) {
   app.use(express.urlencoded());
   app.use(express.json());
+  if (process.env.NODE_ENV == "development") {
+    app.use(
+      cors({
+        origin: "http://localhost:5173",
+        methods: ["POST", "GET"],
+      })
+    );
+  }
 }
